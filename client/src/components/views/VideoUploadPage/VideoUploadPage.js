fb661129418e7df4268ab9f9764bbf8aee95a550
junhyeong <whitelaw7@naver.com>
2021-03-23 15:27:56 +0900

    210323

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,134 +1,196 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'
 import { Typography, Button, Form, message, Input, Icon } from 'antd';
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
+import { useSelector } from "react-redux";
 
-const { TextArea } = Input;
 const { Title } = Typography;
+const { TextArea } = Input;
 
-const PrivateOptions = [
-    {value: 0, label: "Private"},
-    {value: 1, label: "Public"}
+const Private = [
+    { value: 0, label: 'Private' },
+    { value: 1, label: 'Public' }
 ]
 
-const CategoryOptions = [
-    {value: 0, label: "Film & Animaition"},
-    {value: 1, label: "Autos & Vehicles"},
-    {value: 2, label: "Music"},
-    {value: 3, label: "Pets & Animals"}
+const Catogory = [
+    { value: 0, label: "Film & Animation" },
+    { value: 0, label: "Autos & Vehicles" },
+    { value: 0, label: "Music" },
+    { value: 0, label: "Pets & Animals" },
+    { value: 0, label: "Sports" },
 ]
 
-function VideoUploadPage() {
+function VideoUploadPage(props) {
+    const user = useSelector(state => state.user);
 
-    const [VideoTitle, setVideoTitle] = useState("")
-    const [Description, setDescription] = useState("")
-    const [Private, setPrivate] = useState(0)
-    const [Category, setCategory] = useState("Film & Animation")
+    const [title, setTitle] = useState("");
+    const [Description, setDescription] = useState("");
+    const [privacy, setPrivacy] = useState(0)
+    const [Categories, setCategories] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [Thumbnail, setThumbnail] = useState("")
 
-    const onTitleChange = (e) => {
-        setVideoTitle(e.currentTarget.value)
+
+    const handleChangeTitle = (event) => {
+        setTitle(event.currentTarget.value)
     }
 
-    const onDescriptionChange = (e) => {
-        setDescription(e.currentTarget.value)
+    const handleChangeDecsription = (event) => {
+        console.log(event.currentTarget.value)
+
+        setDescription(event.currentTarget.value)
     }
 
-    const onPrivateChange = (e) => {
-        setPrivate(e.currentTarget.value)
+    const handleChangeOne = (event) => {
+        setPrivacy(event.currentTarget.value)
     }
 
-    const onCategoryChange = (e) => {
-        setCategory(e.currentTarget.value)
+    const handleChangeTwo = (event) => {
+        setCategories(event.currentTarget.value)
+    }
+
+    const onSubmit = (event) => {
+
+        event.preventDefault();
+
+        if (user.userData && !user.userData.isAuth) {
+            return alert('Please Log in First')
+        }
+
+        if (title === "" || Description === "" ||
+            Categories === "" || FilePath === "" ||
+            Duration === "" || Thumbnail === "") {
+            return alert('Please first fill all the fields')
+        }
+
+        const variables = {
+            writer: user.userData._id,
+            title: title,
+            description: Description,
+            privacy: privacy,
+            filePath: FilePath,
+            category: Categories,
+            duration: Duration,
+            thumbnail: Thumbnail
+        }
+
+        axios.post('/api/video/uploadVideo', variables)
+            .then(response => {
+                if (response.data.success) {
+                    alert('video Uploaded Successfully')
+                    props.history.push('/')
+                } else {
+                    alert('Failed to upload video')
+                }
+            })
+
     }
 
     const onDrop = (files) => {
-        let formData = new FormData;
-        const config = {
-            header: {'content-type': 'multipart/form-data'}
-        }
-        formData.append("file", files[0])
-        
-        axios.post('/api/video/uploadfiles', formData, config)
-        .then(response => {
-            if (response.data.success) {
 
-            } else {
-                alert('비디오 업로드를 실패했습니다.')
-            }
-        })
-        
+        let formData = new FormData();
+        const config = {
+            header: { 'content-type': 'multipart/form-data' }
+        }
+        console.log(files)
+        formData.append("file", files[0])
+
+        axios.post('/api/video/uploadfiles', formData, config)
+            .then(response => {
+                if (response.data.success) {
+
+                    let variable = {
+                        filePath: response.data.filePath,
+                        fileName: response.data.fileName
+                    }
+                    setFilePath(response.data.filePath)
+
+                    //gerenate thumbnail with this filepath ! 
+
+                    axios.post('/api/video/thumbnail', variable)
+                        .then(response => {
+                            if (response.data.success) {
+                                setDuration(response.data.fileDuration)
+                                setThumbnail(response.data.thumbsFilePath)
+                            } else {
+                                alert('Failed to make the thumbnails');
+                            }
+                        })
+
+
+                } else {
+                    alert('failed to save the video in server')
+                }
+            })
+
     }
 
     return (
-        <div style={{ maxWidth:'700px', margin:'2rem auto'}}>
-            <div style={{ textAlign:'center', marginBottom:'2rm'}}>
-            <Title level={2}>Upload Video</Title>
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+                <Title level={2} > Upload Video</Title>
             </div>
 
-            <Form onsubmit>
-                <div style={{ display:'flex', justifyContent:'space-between'}}>
-                    {/* Drop Zone */}
+            <Form onSubmit={onSubmit}>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Dropzone
-                    onDrop={onDrop}
-                    multiple={false}
-                    maxSize={10000000000}
-                    >
-                    {({ getRootProps, getInputProps}) => (
-                        <div style={{ width: '300px', height: '240px', border:'1px solid lightgray', display:'flex',
-                        alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <Icon type="plus" style={{ fontSize:'3rem'}} />
-                        </div>
+                        onDrop={onDrop}
+                        multiple={false}
+                        maxSize={800000000}>
+                        {({ getRootProps, getInputProps }) => (
+                            <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                                {...getRootProps()}
+                            >
+                                <input {...getInputProps()} />
+                                <Icon type="plus" style={{ fontSize: '3rem' }} />
+
+                            </div>
                         )}
-
                     </Dropzone>
-                    {/* Thumbnail */}
-                    <div>
-                        <img src alt />
-                    </div>
 
+                    {Thumbnail !== "" &&
+                        <div>
+                            <img src={`http://localhost:5000/${Thumbnail}`} alt="haha" />
+                        </div>
+                    }
                 </div>
 
-                <br/>
-                <br/>
+                <br /><br />
                 <label>Title</label>
-                <Input 
-                    onChange={onTitleChange}
-                    value={VideoTitle}
+                <Input
+                    onChange={handleChangeTitle}
+                    value={title}
                 />
-                <br/>
-                <br/>
+                <br /><br />
                 <label>Description</label>
-                <TextArea  
-                    onChange={onDescriptionChange}
+                <TextArea
+                    onChange={handleChangeDecsription}
                     value={Description}
                 />
-                <br/>
-                <br/>
+                <br /><br />
 
-                <select onChange={onPrivateChange}>
-                    {PrivateOptions.map((item, index) => (
+                <select onChange={handleChangeOne}>
+                    {Private.map((item, index) => (
                         <option key={index} value={item.value}>{item.label}</option>
                     ))}
                 </select>
-                <br/>
-                <br/>
-                <select onChange={onCategoryChange}>
-                    {CategoryOptions.map((item, index) => (
-                         <option key={index} value={item.value}>{item.label}</option>
+                <br /><br />
+
+                <select onChange={handleChangeTwo}>
+                    {Catogory.map((item, index) => (
+                        <option key={index} value={item.label}>{item.label}</option>
                     ))}
                 </select>
+                <br /><br />
 
-                <br/>
-                <br/>
-
-                <Button type="primary" size="large" onClick>
+                <Button type="primary" size="large" onClick={onSubmit}>
                     Submit
-                </Button>
+            </Button>
+
             </Form>
-            </div>
+        </div>
     )
 }
 
 export default VideoUploadPage
-
--- server/routes/video.js
+++ server/routes/video.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-// const { Video } = require("../models/Video");
+const multer = require('multer');
+var ffmpeg = require('fluent-ffmpeg');
 
+const { Video } = require("../models/Video");
 const { auth } = require("../middleware/auth");
-const multer = require("multer");
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -25,8 +26,9 @@ var upload = multer({ storage: storage }).single("file")
 
 
 //=================================
-//             Video
+//             User
 //=================================
+
 
 router.post("/uploadfiles", (req, res) => {
 
@@ -38,4 +40,106 @@ router.post("/uploadfiles", (req, res) => {
     })
 
 });
+
+
+router.post("/thumbnail", (req, res) => {
+
+    let thumbsFilePath ="";
+    let fileDuration ="";
+
+    ffmpeg.ffprobe(req.body.filePath, function(err, metadata){
+        console.dir(metadata);
+        console.log(metadata.format.duration);
+
+        fileDuration = metadata.format.duration;
+    })
+
+
+    ffmpeg(req.body.filePath)
+        .on('filenames', function (filenames) {
+            console.log('Will generate ' + filenames.join(', '))
+            thumbsFilePath = "uploads/thumbnails/" + filenames[0];
+        })
+        .on('end', function () {
+            console.log('Screenshots taken');
+            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+        })
+        .screenshots({
+            // Will take screens at 20%, 40%, 60% and 80% of the video
+            count: 3,
+            folder: 'uploads/thumbnails',
+            size:'320x240',
+            // %b input basename ( filename w/o extension )
+            filename:'thumbnail-%b.png'
+        });
+
+});
+
+
+
+
+router.get("/getVideos", (req, res) => {
+
+    Video.find()
+        .populate('writer')
+        .exec((err, videos) => {
+            if(err) return res.status(400).send(err);
+            res.status(200).json({ success: true, videos })
+        })
+
+});
+
+
+
+router.post("/uploadVideo", (req, res) => {
+
+    const video = new Video(req.body)
+
+    video.save((err, video) => {
+        if(err) return res.status(400).json({ success: false, err })
+        return res.status(200).json({
+            success: true 
+        })
+    })
+
+});
+
+
+router.post("/getVideo", (req, res) => {
+
+    Video.findOne({ "_id" : req.body.videoId })
+    .populate('writer')
+    .exec((err, video) => {
+        if(err) return res.status(400).send(err);
+        res.status(200).json({ success: true, video })
+    })
+});
+
+
+router.post("/getSubscriptionVideos", (req, res) => {
+
+
+    //Need to find all of the Users that I am subscribing to From Subscriber Collection 
+    
+    Subscriber.find({ 'userFrom': req.body.userFrom })
+    .exec((err, subscribers)=> {
+        if(err) return res.status(400).send(err);
+
+        let subscribedUser = [];
+
+        subscribers.map((subscriber, i)=> {
+            subscribedUser.push(subscriber.userTo)
+        })
+
+
+        //Need to Fetch all of the Videos that belong to the Users that I found in previous step. 
+        Video.find({ writer: { $in: subscribedUser }})
+            .populate('writer')
+            .exec((err, videos) => {
+                if(err) return res.status(400).send(err);
+                res.status(200).json({ success: true, videos })
+            })
+    })
+});
+
 module.exports = router;
